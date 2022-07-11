921bc040efab3b8efe3dfa7c1fdede62ea0f8aaf
LakshmiNarasimha Vakkalanka <lakshminarasimha.vakkalanka@infor.com>
2022-07-12 01:20:04 +0530

    filtering data

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@ using System.Net.Http;
 using ProcessSharePoint.Entities;
 using IdentityModel.Client;
 using System.Linq;
+using System;
+using System.Net;
 
 namespace ProcessSharePoint
 {
@@ -52,23 +54,42 @@ namespace ProcessSharePoint
             response = httpClient.GetAsync(SiteDataEndPoint).Result;
             var siteData = response.Content.ReadAsStringAsync().Result;
             var sharepointSite = JsonConvert.DeserializeObject<SharePointSite>(siteData);
-            ;
+            
 
             var ListsEndPoint = _configuration["SharePointOnline:ListsEndPoint"];
             ListsEndPoint = string.Format(ListsEndPoint, sharepointSite.id);
 
-            ;
+            
             httpClient.SetBearerToken(accessToken);
             response = httpClient.GetAsync(ListsEndPoint).Result;
             var listData = response.Content.ReadAsStringAsync().Result;
             var sharePointList = JsonConvert.DeserializeObject<SharePointList>(listData);
-            var listid = sharePointList.value.FirstOrDefault(obj => obj.displayName == "Tasks").id;
+            var listid = sharePointList.value.FirstOrDefault(obj => obj.displayName == "Infor Services Bidpacks").id;
             
 
-            var ListDataEndPoint = _configuration["SharePointOnline:ListDataEndPoint"];
+            var ListDataEndPoint = _configuration["SharePointOnline:ListDataByFilter"];
             ListDataEndPoint = string.Format(ListDataEndPoint, sharepointSite.id, listid);
             httpClient.SetBearerToken(accessToken);
             response = httpClient.GetAsync(ListDataEndPoint).Result;
+
+            //Below logic is to handle TooManyRequests Error. We wait for seconds mentioned in Header with name "Retry-After" and try to call the endpoint again.
+            int maxRetryCount = 3;
+            int retriesCount = 0;
+
+            if (response.StatusCode == HttpStatusCode.TooManyRequests)
+            {
+                do
+                {
+                    // Determine the retry after value - use the "Retry-After" header
+                    var retryAfterInterval = Int32.Parse(response.Headers.GetValues("Retry-After").FirstOrDefault());
+
+                    //we get retryAfterInterval in seconds. We need to pass milliseconds to Thread.Sleep method, hence we multiply retryAfterInterval with 1000
+                    System.Threading.Thread.Sleep(retryAfterInterval * 1000);
+                    response = httpClient.GetAsync(ListDataEndPoint).Result;
+                    retriesCount += 1;
+                } while (response.StatusCode == HttpStatusCode.TooManyRequests && retriesCount <= maxRetryCount);
+            }
+
             var ListData = response.Content.ReadAsStringAsync().Result;
 
 
